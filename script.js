2aca78becce3c067ad6c015897bcc36cf284c379
PeterKallmanCode <163829221+PeterKallmanCode@users.noreply.github.com>
2025-11-19 18:13:41 +0100

    min labb

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-const checkbox = document.getElementsByClassName('checkbox');
+const checkbox = document.getElementsByName('divstyle')[0];
 console.log(checkbox);
+
+
+const textfield = document.getElementsByClassName('textfield');
+console.log(textfield);
+
+
+const button = document.getElementById('btn');
+console.log(button);
+
+
+const div = document.querySelector('div');
+console.log(div);
+
+
+
+
+
+
+// Den här fungerar som arrowfunktion
+// Göra om arrowfunktionen till en "funktionsdeklaration"
+
+// addEventListener('input', e => {
+//     if (e.target.id === "content")
+//     console.log('target');
+// });
