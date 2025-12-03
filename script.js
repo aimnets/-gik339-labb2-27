0042cd19de98ea43f4c08e5e2cfbc0479bc484b4
Marcus Lindström <marcus2002@hotmail.se>
2025-12-03 17:12:40 +0100

    Refactor event listener syntax and variable usage

--- script.js
+++ script.js
@@ -1,49 +1,36 @@
-const checkbox = document.getElementsByName('divstyle')[0];
-const textfield = document.getElementsByClassName('textfield');
-const button = document.getElementById('btn');
-const div = document.querySelector('div');
+const checkbox = document.getElementsByName("divstyle")[0];
+const textfield = document.getElementsByClassName("textfield")[0];
+const button = document.getElementById("btn");
+const div = document.querySelector("div");
 
 console.log(checkbox);
 console.log(textfield);
 console.log(button.innerHTML);
 console.log(div.innerHTML);
 
-var inputColor = "";
-var inputContent = "";
-
 function handleInput(e) {
-    if (e.target.id === "content") {
-        inputContent = e.target.value;
-        console.log(e.target);
-        return;
-    }
-    if (e.target.name === "content") {
-        inputContent = e.target.value;
-        inputContent = div.innerHTML;
-        return inputContent;
-    }
-        if (e.target.name === "color") {
-        return inputColor = e.target.value;
-    }
+  console.log(e.target);
+  if (e.target.id === "content") {
+    inputContent = e.target.value;
+    div.innerHTML = inputContent;
+  }
+  if (e.target.name === "color") {
+    inputColor = e.target.value;
+  }
 }
-addEventListener('input', handleInput);
-
+addEventListener("input", handleInput);
 
 function checkboxChange(e) {
-    if (e.target.id === "divstyle") {
-        div.style.backgroundColor = inputColor;
-    }
+  if (e.target.id === checkbox.id) {
+    div.style.backgroundColor = inputColor;
+  }
 }
-addEventListener('change', checkboxChange);
+addEventListener("change", checkboxChange);
 
-// Förstå hur man tar bort div elementet, inte bara ta bort innehållet.
 function buttonPress(e) {
-    if (e.target.id === "btn") {
-        console.log("button pressed");
-        div.remove();
-        /*div.innerHTML = "";
-        div.style.backgroundColor = "";*/
-    }
+  if (e.target.id === button.id) {
+    console.log("button pressed");
+    div.remove();
+  }
 }
-addEventListener('click', buttonPress);
-
+addEventListener("click", buttonPress);
